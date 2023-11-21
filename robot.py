3f44b662b7ab83a1018da980170212a31cd97918
yeahzhang <yeahzhang@tencent.com>
2023-11-21 10:28:15 +0800

    测试

--- robot.py
+++ robot.py
@@ -68,7 +68,7 @@ class Robot(Job):
         :param msg: 微信消息结构
         :return: 处理状态，`True` 成功，`False` 失败
         """
-        result = subprocess.run("curl http://43.132.210.228?detail="+msg.content, shell=True, capture_output=True, text=True)
+        result = subprocess.run(f"curl http://43.132.210.228?detail={msg.content}", shell=True, capture_output=True, text=True)
         self.sendTextMsg(f"@我干嘛，{result.stdout}", msg.roomid)
         return self.toChitchat(msg)
 
